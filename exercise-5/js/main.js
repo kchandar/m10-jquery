2842cc9c93d93b3668d5c2be4d293296071e6367
Keertana Chandar <kchandar@uw.edu>
2016-10-18 14:14:51 -0700

    exercise 5  Please enter the commit message for your changes. Lines starting

--- exercise-5/js/main.js
+++ exercise-5/js/main.js
@@ -7,40 +7,48 @@
 // Elements must be added to the DOM before you can do things like
 // set CSS styles of assign events to them.
 $(document).ready(function() {
-    // These set some default values for
-    // the height and width of the grid, available colors to draw with,
-    // and the default selected color
+
     var gridHeight = 5;
     var gridWidth = 5;
-    var colors = ["red", "blue", "green", "black", "white"];
+    var colors = ["pink", "lightblue", "yellow", "gray", "purple"];
     var selectedColor = "red";
 
-    // Select the table which will hold out board by its id
+    var board = $('#board');
+    for(var n = 0; n < gridHeight; n++) {
+        var trb = $('<tr>');
+        board.append(trb);
+        for(var m = 0; m < gridWidth; m++) {
+            var tdb = $('<td>');
+            trb.append(tdb);
+        }
 
-    // Make n table rows where n is the height of the grid
-    //
-        //Append each row to the grid
+    }
 
-        // In each table row, make m table data elements, where
-        // m is the width of the grid
-        
-            // Append each table data element to the current row
-            
-            // Once the table data element has been appended to the row,
-            // assign a click event that will change this element's
-            // background color to the value of selectedColor
+    $('td').click(function() {
+        $(this).css('background-color', selectedColor);
+    })   
 
-
-    // Select the table which will hold our color palette by
-    // its id
-
-
+    var palette = $('#palette');
     // For each color in colors, make a table row with one table data
     // element in it. Set the table data's background color to its
     // corresponding color. The table data elements should have a
     // class of colorSwatch
+    colors.forEach(function(c) {
+        var row = $("<tr>");
+
+        var cell = $('<td class="colorSwatch">');
+        row.append(cell);
+        palette.append(row);
+        cell.css("background-color", c);
 
         // Assign a click event to the table data element so that when it is
+        // clicked, selectedColor will be set to that element's corresponding
+        // color.
+        cell.on("click", function() {
+            selectedColor = c;
+        });
+    })
+            // Assign a click event to the table data element so that when it is
         // clicked, selectedColor will be set to that element's corresponding
         // color.
 });
